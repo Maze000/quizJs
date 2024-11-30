4a874a7499d35b87b4e5430e72ff4c19bb05c222
Maze <infimo000@gmail.com>
2024-11-30 17:00:44 +1000

    mod quiz

---
+++ data/data.js
@@ -0,0 +1,57 @@
+export const data = [
+  {
+    question: "Which of the following is not a code editor?",
+    choices: ["vim", "vscode", "emacs", "excel"],
+    answer: "excel",
+  },
+  {
+    question: "Which language is not object-oriented?",
+    choices: ["Java", "Haskell", "C++", "Python"],
+    answer: "Haskell",
+  },
+  {
+    question: "Which language is not used for styling websites?",
+    choices: ["Stylus", "Less", "Sass", "JavaScript"],
+    answer: "JavaScript",
+  },
+  {
+    question: "Which data structure uses a LIFO (Last In, First Out) approach?",
+    choices: ["Queue", "Stack", "Array", "Linked List"],
+    answer: "Stack",
+  },
+  {
+    question: "Which of the following is not a JavaScript framework?",
+    choices: ["React", "Angular", "Vue", "Django"],
+    answer: "Django",
+  },
+  {
+    question: "What does SQL stand for?",
+    choices: [
+      "Structured Query Language",
+      "Simple Query Language",
+      "Structured Queue Language",
+      "Simplified Query Language",
+    ],
+    answer: "Structured Query Language",
+  },
+  {
+    question: "Which method is used to add an element to the end of an array in JavaScript?",
+    choices: ["push()", "pop()", "shift()", "unshift()"],
+    answer: "push()",
+  },
+  {
+    question: "Which HTTP method is used to update data on a server?",
+    choices: ["GET", "POST", "PUT", "DELETE"],
+    answer: "PUT",
+  },
+  {
+    question: "Which of the following is not a primitive data type in JavaScript?",
+    choices: ["string", "boolean", "object", "number"],
+    answer: "object",
+  },
+  {
+    question: "What is the file extension for a React component file?",
+    choices: [".jsx", ".js", ".json", ".react"],
+    answer: ".jsx",
+  },
+];
